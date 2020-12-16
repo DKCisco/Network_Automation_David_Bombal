9e7bcf133af6cfee979e479f1954020939af5bf9
DKCisco <73546877+DKCisco@users.noreply.github.com>
2020-12-15 18:07:02 -0700

    Add files via upload

--- telnet_gns3.py
+++ telnet_gns3.py
@@ -1,25 +1,24 @@
 import getpass
 import telnetlib
 
-HOST = input("Enter the node ip: ")
-user = raw_input("Enter your remote account: ")
+HOST = input("Enter the host address: ")
+user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
 tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
+tn.write(user.encode('ascii') + b"\r\n")
 if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\r\n")
 
 tn.write(b"enable\n")
-tn.write(b"test123A\n")
+tn.write(b"cisco\n")
 tn.write(b"conf t\n")
 tn.write(b"int loop 0\n")
-tn.write(b"ip addresss 1.1.1.1 255.255.255.255\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
 tn.write(b"end\n")
-tn.write(b"wr er\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
